bc3220792d16c0d16a74e871447f09d88b0b0d92
Alexey Ershkov <a.ershkov@corp.mail.ru>
2021-10-13 23:49:37 +0300

    improved hw-1

--- project/src/main.c
+++ project/src/main.c
@@ -46,7 +46,7 @@ int main(int argc, const char** argv) {
                 // int res = custom_pow(base, pow);    // TODO: Implement me
 
                 // printf("%i\n", res);
-                printf("Hello world!");
+                printf("Hello world! !!!!!");
             } else {
                 return ERR_ARGS_COUNT;
             }
